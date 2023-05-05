a1c385577085b8f9167024aedd036a32049f398e
Leila <leiisalguero@gmaill.com>
2023-05-05 19:04:39 -0300

    add changes for deploy

--- Server/src/app.js
+++ Server/src/app.js
@@ -10,11 +10,11 @@ const { signUpRoute } = require('./routes/signUpRoute.js');
 const server = express();
 
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://rick-and-morty-app.up.railway.app');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header(
        'Access-Control-Allow-Headers',
-       'Origin, X-Requested-With, Content-Type, Accept'
+       '*'
     )
     res.header(
        'Access-Control-Allow-Methods',
